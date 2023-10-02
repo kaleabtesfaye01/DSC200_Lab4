8bb129e7d088c0e0ff51f7c43f53bf9b1aa08564
Kaleab Alemu <kaleabtesfaye01@gmail.com>
2023-10-02 15:06:40 -0400

    completed get_values function

--- 2_Lab4.py
+++ 2_Lab4.py
@@ -1,4 +1,5 @@
-import openpyxl
+import openpyxl as op
+import csv
 
 
 def get_categories(ws):
@@ -6,7 +7,15 @@ def get_categories(ws):
 
 
 def get_values(ws):
-    pass
+    values = []
+
+    for row in ws['E15:AF223']:
+        value = []
+        for cell in row:
+            if type(cell.value) == int or type(cell.value) == float or cell.value == chr(0x2013):
+                value.append(cell.value)
+        values.append(value)
+    return values
 
 
 def get_countries(ws):
@@ -18,4 +27,10 @@ def write_csv(countries, categories, values):
 
 
 def main():
-    pass
+    wb = op.load_workbook('./data/Lab4Data.xlsx', read_only=True, data_only=True)
+
+    ws = wb.active
+
+    get_values(ws)
+
+main()
