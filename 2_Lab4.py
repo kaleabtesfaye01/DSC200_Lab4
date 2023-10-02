cda592adafad8d998362745c5f335712bc5a1576
Kaleab Alemu <kaleabtesfaye01@gmail.com>
2023-10-02 13:14:55 -0400

    create template .py file

---
+++ 2_Lab4.py
@@ -0,0 +1,21 @@
+import openpyxl
+
+
+def get_categories(ws):
+    pass
+
+
+def get_values(ws):
+    pass
+
+
+def get_countries(ws):
+    pass
+
+
+def write_csv(countries, categories, values):
+    pass
+
+
+def main():
+    pass
