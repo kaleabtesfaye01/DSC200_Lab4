9d39f7fb0843cfb7d717a6476c14fad939a29bd0
Manogya Aryal <manogyaaryal39@gmail.com>
2023-10-04 13:01:21 -0400

    Added get categories

--- 2_Lab4.py
+++ 2_Lab4.py
@@ -13,38 +13,63 @@ import openpyxl as op
 import csv
 
 
-def get_categories(ws):
-    pass
+class DSCLab5:
+    def __init__(self,csvFile):
+        self.csvFile = csvFile
+
+    def get_categories(ws):
+        category_names = []
+        for cols in ws.iter_rows(min_row=5, max_row=7, min_col=5, max_col=31):
+            row = []
+            for cell in cols:
+                currVal = cell.value
+                for merged_cells in ws.merged_cells.ranges:
+                    if cell.coordinate in merged_cells:
+                        currVal = merged_cells.start_cell.value
+                if cell.row == 6 and currVal in category_names[0]:
+                    currVal = ''
+                row.append(currVal)
+            category_names.append(row)
+
+        categories = []
+        for i in range(len(category_names[0])):
+            category = ''
+            for j in range(len(category_names)):
+                category_names[j][i] = category_names[j][i].replace('\n', '')
+                category = category + '_' + category_names[j][i] if category != '' else category_names[j][i]
+            categories.append(category)
+
+        return list(dict.fromkeys(categories))
 
 
-# This function "get_values", given the worksheet "Table 9" as a parameter, returns a list of lists containing all the
-# values for each category of child abuse for each country
-def get_values(ws):
-    values = list()  # initialize our values list to an empty list
+    # This function "get_values", given the worksheet "Table 9" as a parameter, returns a list of lists containing all the
+    # values for each category of child abuse for each country
+    def get_values(ws):
+        values = list()  # initialize our values list to an empty list
 
-    for row in ws['E15:AF211']:  # iterate through cells E15:AF211 to extract all the relevant values
-        value = []  # holds the list of values for one country
-        for cell in row:  # iterate through cells in each row
-            # if the value is an integer or a floating point number or an en dash, we add it to our list of values
-            if type(cell.value) == int or type(cell.value) == float or cell.value == chr(0x2013):
-                value.append(cell.value)
-        values.append(value)  # append the values in one row into the values list
-    return values
-
-
-# This function "get_countries", given the worksheet "Table 9" as a parameter, returns the list of countries in the
-# worksheet
-def get_countries(ws):
-    countries = list()  # initialize our countries list to an empty list
-
-    for row in ws['B15:B211']:  # iterate through cells B15:B211 to extract the country names
-        for cell in row:  # iterate though the cells in each row
-            countries.append(cell.value)  # add the value of each cell into our countries list
-    return countries
+        for row in ws['E15:AF211']:  # iterate through cells E15:AF211 to extract all the relevant values
+            value = []  # holds the list of values for one country
+            for cell in row:  # iterate through cells in each row
+                # if the value is an integer or a floating point number or an en dash, we add it to our list of values
+                if type(cell.value) == int or type(cell.value) == float or cell.value == chr(0x2013):
+                    value.append(cell.value)
+            values.append(value)  # append the values in one row into the values list
+        return values
 
 
-def write_csv(countries, categories, values):
-    pass
+    # This function "get_countries", given the worksheet "Table 9" as a parameter, returns the list of countries in the
+    # worksheet
+    def get_countries(ws):
+        countries = list()  # initialize our countries list to an empty list
+
+        for row in ws['B15:B211']:  # iterate through cells B15:B211 to extract the country names
+            for cell in row:  # iterate though the cells in each row
+                countries.append(cell.value)  # add the value of each cell into our countries list
+        return countries
+
+
+    def write_csv(countries, categories, values):
+        pass
 
 
 def main():
